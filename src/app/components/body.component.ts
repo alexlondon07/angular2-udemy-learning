54d067f3c478fe73f22f3b81ef6a6fba8f3ed3cf
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2017-03-20 11:36:20 -0500

    directivas estructurales ngif y ngfor

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
+    show:boolean = true;
+    text:string = "Mi ambición ha sido siempre hacer realizables los sueños.";
+    author:string = "Bill Gates";
+
+
+    personajes:string[] = ["Spiderman" , "Thor", "Batman", "Capitan America", "Wolverin"];
 }
